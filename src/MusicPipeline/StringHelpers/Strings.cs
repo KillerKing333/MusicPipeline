351c11f3f6454954260701ddfe0698d0bfde076b
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-09-09 19:41:02 +0100

    OMG EXTRA >

--- src/MusicPipeline/StringHelpers/Strings.cs
+++ src/MusicPipeline/StringHelpers/Strings.cs
@@ -5,7 +5,7 @@ public class StringHelpers
 	public static async Task<Dictionary<int, string>> SplitLinesDict(string allText)
 	{
 		List<string> lines = new(allText.Split("\n"));
-		Dictionary<int, string>> res = new();
+		Dictionary<int, string> res = new();
 		for (int i = 0; i < (lines.Count()); i++) {
 			res.Append(new KeyValuePair<int, string>(i, lines[i]));
 		}
