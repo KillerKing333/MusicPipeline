fee6b68cade3dcc20babc4f3b15b2d0561177d7b
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-09-09 19:38:07 +0100

    Forgot a semicolon because sublime was literally highlighting every other line with an error

--- src/MusicPipeline/StringHelpers/Strings.cs
+++ src/MusicPipeline/StringHelpers/Strings.cs
@@ -7,7 +7,7 @@ public class StringHelpers
 		List<string> lines = new(allText.Split("\n"));
 		Dictionary<int, string>> res = new();
 		for (int i = 0; i < (lines.Count()); i++) {
-			res.Append(new KeyValuePair<int, string>(i, lines[i]))
+			res.Append(new KeyValuePair<int, string>(i, lines[i]));
 		}
 		return res;
 	}
