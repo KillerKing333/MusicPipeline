2b9f8571cc57ab20aca137db4307c1cf5ef44e86
Skitty Aylieal <114876160+skittyaylieal@users.noreply.github.com>
2026-09-12 18:36:21 +0100

    Even more debugging

--- src/MusicPipeline/StringHelpers/Strings.cs
+++ src/MusicPipeline/StringHelpers/Strings.cs
@@ -5,10 +5,13 @@ public class StringHelpers
 	public static async Task<Dictionary<int, string>> SplitLinesDict(string allText)
 	{
 		List<string> lines = new(allText.Split("\n"));
+		Console.WriteLine($"lines is {lines}, count is {lines.Count()}, allText length is {allText.Length}");
 		Dictionary<int, string> res = new();
 		for (int i = 0; i < (lines.Count()); i++) {
 			res.Append(new KeyValuePair<int, string>(i, lines[i]));
+			Console.WriteLine($"New line, {lines[i]}");
 		}
+		Console.WriteLine(res);
 		return res;
 	}
 }
